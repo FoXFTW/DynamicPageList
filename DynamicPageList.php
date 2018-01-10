42b26b22197d4405f1ac6eb46d4f456d96d4b380
FoXFTW <hannes@octofox.de>
2018-01-10 20:04:15 +0100

    :fire: Remove unnecessary whitespace

--- DynamicPageList.php
+++ DynamicPageList.php
@@ -18,6 +18,6 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	);
 	return;
- } else {
+} else {
 	die( 'This version of the DynamicPageList extension requires MediaWiki 1.25+' );
 }
