17b8d01be8bb4f3bd4f7bb33189b94d7d9975b8f
FoXFTW <hannes@octofox.de>
2018-01-12 00:37:40 +0100

    :memo: Update PHPDoc; :hammer: Update formatting

--- classes/Config.php
+++ classes/Config.php
@@ -3,11 +3,12 @@
  * DynamicPageList3
  * DPL Config Class
  *
- * @author		IlyaHaykinson, Unendlich, Dangerville, Algorithmix, Theaitetos, Alexia E. Smith
- * @license		GPL
- * @package		DynamicPageList3
+ * @author        IlyaHaykinson, Unendlich, Dangerville, Algorithmix, Theaitetos, Alexia E. Smith
+ * @license        GPL
+ * @package        DynamicPageList3
  *
  **/
+
 namespace DPL;
 
 use MWException;
@@ -16,48 +17,45 @@ class Config {
 	/**
 	 * Configuration Settings
 	 *
-	 * @var		array
+	 * @var array
 	 */
 	static private $settings = [];
 
 	/**
 	 * Initialize the static object with settings.
 	 *
-	 * @access    public
-	 * @param    bool    Settings to initialize for DPL.
-	 * @return    void
-	 * @throws MWException
+	 * @param bool $settings Settings to initialize for DPL.
+	 * @return void
+	 * @throws \MWException
 	 */
-	static public function init($settings = false) {
-		if ($settings === false) {
+	static public function init( $settings = false ) {
+		if ( $settings === false ) {
 			global $wgDplSettings;
 
 			$settings = $wgDplSettings;
 		}
 
-		if (!is_array($settings)) {
-			throw new MWException(__METHOD__.": Invalid settings passed.");
+		if ( !is_array( $settings ) ) {
+			throw new MWException( __METHOD__ . ": Invalid settings passed." );
 		}
 
-		self::$settings = array_merge(self::$settings, $settings);
+		self::$settings = array_merge( self::$settings, $settings );
 	}
 
 	/**
 	 * Return a single setting.
 	 *
-	 * @access	public
-	 * @param	string	Setting Key
-	 * @return	mixed	The setting's actual setting or null if it does not exist.
+	 * @param string $setting Setting Key
+	 * @return mixed The setting's actual setting or null if it does not exist.
 	 */
-	static public function getSetting($setting) {
-		return (array_key_exists($setting, self::$settings) ? self::$settings[$setting] : null);
+	static public function getSetting( $setting ) {
+		return ( array_key_exists( $setting, self::$settings ) ? self::$settings[$setting] : null );
 	}
 
 	/**
 	 * Return a all settings.
 	 *
-	 * @access	public
-	 * @return	array	All settings
+	 * @return array All settings
 	 */
 	static public function getAllSettings() {
 		return self::$settings;
@@ -66,15 +64,14 @@ class Config {
 	/**
 	 * Set a single setting.
 	 *
-	 * @access    public
-	 * @param    string    Setting Key
-	 * @param    mixed    [Optional] Appropriate value for the setting key.
-	 * @return    void
-	 * @throws MWException
+	 * @param string $setting Setting Key
+	 * @param mixed $value [Optional] Appropriate value for the setting key.
+	 * @return void
+	 * @throws \MWException
 	 */
-	static public function setSetting($setting, $value = null) {
-		if (empty($setting) || !is_string($setting)) {
-			throw new MWException(__METHOD__.": Setting keys can not be blank.");
+	static public function setSetting( $setting, $value = null ) {
+		if ( empty( $setting ) || !is_string( $setting ) ) {
+			throw new MWException( __METHOD__ . ": Setting keys can not be blank." );
 		}
 		self::$settings[$setting] = $value;
 	}
