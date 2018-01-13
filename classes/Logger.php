79a521fc0309b2525049c1188e9e2ca2c7bd3d1a
FoXFTW <hannes@octofox.de>
2018-01-13 16:22:15 +0100

    :hammer: DynamicPageListHooks -> ErrorCodes

--- classes/Logger.php
+++ classes/Logger.php
@@ -92,19 +92,19 @@ class Logger {
 	 */
 	private function getMessageTextForErrorId( $errorId, array $messageParameters ) {
 		switch ( $errorId ) {
-			case DynamicPageListHooks::FATAL_TOOMANYCATS:
+			case ErrorCodes::FATAL_TOO_MANY_CATEGORIES:
 				$text = wfMessage( 'intersection_toomanycats', $messageParameters )->text();
 				break;
 
-			case DynamicPageListHooks::FATAL_TOOFEWCATS:
+			case ErrorCodes::FATAL_TOO_FEW_CATEGORIES:
 				$text = wfMessage( 'intersection_toofewcats', $messageParameters )->text();
 				break;
 
-			case DynamicPageListHooks::WARN_NORESULTS:
+			case ErrorCodes::WARN_NO_RESULTS:
 				$text = wfMessage( 'intersection_noresults', $messageParameters )->text();
 				break;
 
-			case DynamicPageListHooks::FATAL_NOSELECTION;
+			case ErrorCodes::FATAL_NO_SELECTION;
 				$text = wfMessage( 'intersection_noincludecats', $messageParameters )->text();
 				break;
 
