831893b36b7dbb21a3493c2a700aa411b5420962
FoXFTW <hannes@octofox.de>
2018-01-13 14:30:47 +0100

    :hammer: Refactor ListMode, split constructor into multiple methods

--- classes/ListMode.php
+++ classes/ListMode.php
@@ -70,6 +70,14 @@ class ListMode {
 	public $iDominantSection = - 1;
 
 	/**
+	 * Holds listAttributes in 'list' key and itemAttributes in 'item' key
+	 * Used in __construct
+	 *
+	 * @var array
+	 */
+	private $listItemAttributes = [];
+
+	/**
 	 * Main Constructor
 	 *
 	 * @param string $listMode List Mode
@@ -93,78 +101,158 @@ class ListMode {
 		}
 
 		$this->name = $listMode;
-		$_listAttributes =
-			( $listAttributes == '' ) ? '' : ' ' . Sanitizer::fixTagAttributes( $listAttributes, 'ul' );
-		$_itemAttributes =
-			( $itemAttributes == '' ) ? '' : ' ' . Sanitizer::fixTagAttributes( $itemAttributes, 'li' );
-
 		$this->sSectionTags = $sectionSeparators;
 		$this->aMultiSecSeparators = $multiSectionSeparators;
 		$this->iDominantSection = $dominantSection - 1; // 0 based index
 
-		switch ( strtolower($listMode) ) {
-			case 'inline':
-				if ( stristr( $inlineText, '<br />' ) ) { //one item per line (pseudo-inline)
-					$this->sListStart = "<div {$_listAttributes}>";
-					$this->sListEnd = '</div>';
-				}
+		$this->listItemAttributes = [
+			'list' => $this->sanitizeAttribute( $listAttributes, 'ul' ),
+			'item' => $this->sanitizeAttribute( $itemAttributes, 'li' ),
+		];
 
-				$this->sItemStart = "<span {$_itemAttributes}>";
-				$this->sItemEnd = '</span>';
-				$this->sInline = $inlineText;
+		switch ( strtolower( $listMode ) ) {
+			case 'inline':
+				$this->setInlineMode( $inlineText );
 				break;
 
 			case 'gallery':
-				$this->sListStart = "<gallery>\n";
-				$this->sListEnd = "\n</gallery>";
-				$this->sItemStart = '';
-				$this->sItemEnd = '||';
-				$this->sInline = "\n";
+				$this->setGalleryMode();
 				break;
 
 			case 'ordered':
-				if ( $iOffset == 0 ) {
-					$this->sListStart = "<ol start=1 {$_listAttributes}>";
-				} else {
-					$iOffset++;
-					$this->sListStart = "<ol start={$iOffset} {$_listAttributes}>";
-				}
-
-				$this->sListEnd = '</ol>';
-				$this->sItemStart = "<li {$_itemAttributes}>";
-				$this->sItemEnd = '</li>';
+				$this->setOrderedMode( $iOffset );
 				break;
 
 			case 'unordered':
-				$this->sListStart = "<ul {$_listAttributes}>";
-				$this->sListEnd = '</ul>';
-				$this->sItemStart = "<li {$_itemAttributes}>";
-				$this->sItemEnd = '</li>';
+				$this->setUnorderedMode();
 				break;
 
 			case 'definition':
-				$this->sListStart = "<dl {$_listAttributes}>";
-				$this->sListEnd = '</dl>';
-				// item html attributes on dt element or dd element ?
-				$this->sHeadingStart = '<dt>';
-				$this->sHeadingEnd = '</dt><dd>';
-				$this->sItemEnd = '</dd>';
+				$this->setDefinitionMode();
 				break;
 
 			case 'h2':
 			case 'h3':
 			case 'h4':
-				$this->sListStart = "<div {$_listAttributes}>";
-				$this->sListEnd = '</div>';
-				$this->sHeadingStart = "<{$listMode}>";
-				$this->sHeadingEnd = "</{$listMode}>";
+				$this->setHeadingMode( $listMode );
 				break;
 
 			case 'userformat':
-				list( $this->sListStart, $this->sItemStart, $this->sItemEnd, $this->sListEnd ) =
-					array_pad( $listSeparators, 4, null );
-				$this->sInline = $inlineText;
+				$this->setUserFormatMode( $listSeparators, $inlineText );
 				break;
 		}
+	}
+
+	/**
+	 * Wrapper for Sanitizer::fixTagAttributes
+	 * Returns empty string if attribute is empty
+	 *
+	 * @param string $attribute The Attribute
+	 * @param string $tagType Tag Type
+	 * @return string Sanitized Attribute
+	 */
+	private function sanitizeAttribute( $attribute, $tagType ) {
+		if ( !empty( $attribute ) ) {
+			$attribute = Sanitizer::fixTagAttributes( $attribute, $tagType );
+
+			return " {$attribute}";
+		} else {
+			return '';
+		}
+	}
+
+	/**
+	 * Sets attributes for Inline Mode
+	 * If '<br />' is present in $inlineText, List will be wrapped in divs
+	 *
+	 * @param string $inlineText
+	 */
+	private function setInlineMode( $inlineText ) {
+		if ( stristr( $inlineText, '<br />' ) ) { //one item per line (pseudo-inline)
+			$this->sListStart = "<div {$this->listItemAttributes['list']}>";
+			$this->sListEnd = '</div>';
+		}
+
+		$this->sItemStart = "<span {$this->listItemAttributes['item']}>";
+		$this->sItemEnd = '</span>';
+		$this->sInline = $inlineText;
+	}
+
+	/**
+	 * Sets attributes for Gallery Mode
+	 */
+	private function setGalleryMode() {
+		$this->sListStart = "<gallery>\n";
+		$this->sListEnd = "\n</gallery>";
+		$this->sItemStart = '';
+		$this->sItemEnd = '||';
+		$this->sInline = "\n";
+	}
+
+	/**
+	 * Sets attributes for Ordered List Mode
+	 * If Offset is 0 list starts at 1
+	 *
+	 * @param int $offset Starting offset
+	 */
+	private function setOrderedMode( $offset ) {
+		if ( intval( $offset ) === 0 ) {
+			$this->sListStart = "<ol start=1 {$this->listItemAttributes['list']}>";
+		} else {
+			$offset ++;
+			$this->sListStart = "<ol start={$offset} {$this->listItemAttributes['list']}>";
+		}
+
+		$this->sListEnd = '</ol>';
+		$this->sItemStart = "<li {$this->listItemAttributes['item']}>";
+		$this->sItemEnd = '</li>';
+	}
+
+	/**
+	 * Sets attributes for Unordered List Mode
+	 */
+	private function setUnorderedMode() {
+		$this->sListStart = "<ul {$this->listItemAttributes['list']}>";
+		$this->sListEnd = '</ul>';
+		$this->sItemStart = "<li {$this->listItemAttributes['item']}>";
+		$this->sItemEnd = '</li>';
+	}
+
+	/**
+	 * Sets attributes for Definition List Mode
+	 */
+	private function setDefinitionMode() {
+		$this->sListStart = "<dl {$this->listItemAttributes['list']}>";
+		$this->sListEnd = '</dl>';
+		$this->sHeadingStart = "<dt {$this->listItemAttributes['item']}>";
+		$this->sHeadingEnd = "</dt><dd {$this->listItemAttributes['item']}>";
+		$this->sItemEnd = '</dd>';
+	}
+
+	/**
+	 * Sets attributes for Heading List Mode
+	 *
+	 * @param string $listMode Tag Name
+	 */
+	private function setHeadingMode( $listMode ) {
+		$this->sListStart = "<div {$this->listItemAttributes['list']}>";
+		$this->sListEnd = '</div>';
+		$this->sHeadingStart = "<{$listMode}>";
+		$this->sHeadingEnd = "</{$listMode}>";
+	}
+
+	/**
+	 * Sets sListStart to listSeparators[0] or null,
+	 * sItemStart to listSeparators[1] or null,
+	 * sItemEnd to listSeparators[2] or null,
+	 * sListEnd to listSeparators[3] or null
+	 *
+	 * @param array $listSeparators
+	 * @param string $inlineText
+	 */
+	private function setUserFormatMode( $listSeparators, $inlineText ) {
+		list( $this->sListStart, $this->sItemStart, $this->sItemEnd, $this->sListEnd ) =
+			array_pad( $listSeparators, 4, null );
+		$this->sInline = $inlineText;
 	}
 }
